868f92592e0cc096b31a405c34b8ae05c75871ee
Oeyvind_Brandtsegg <obrandts@gmail.com>
2014-01-09 15:03:14 +0100

    * changed language to a collection of python keywords that must be assembled in a specific sequence * changed fitness: longer statements that evaluates is better * testing all combinations within a chromosome, increment fitness score for each segment that evaluates

--- self_dot.py
+++ self_dot.py
@@ -1,29 +1,61 @@
-# First version of self-generating code: self_dot
+# Attempt at self-generating code: self_dot
 # 
-# Author: Axel Tidemann, axel.tidemann@gmail.com
+# Author: Axel Tidemann and Oeyvind Brandtsegg, (axel.tidemann@gmail.com, obrandts@gmail.com)
 
 from pyevolve import G1DList, GSimpleGA, Mutators
 
-language = [ '1.0', '3.0', '+', '*', '4.0', '.5', '/', ' ' ]
+i = 0
+n = 20
+#language = [ 'for ', 'i ', 'in ', 'range ', 'n ', 'print ', ' ', '(', ')', ':']
+language = [ 'range', '(', 'n', ')']
+
+def combinations(L):
+    combos = []
+    for i in range(len(L)):
+        for j in range(len(L)):
+	        if j+i < len(L):
+		        combos.append(L[j:j+i+1])
+    return combos
 
 def chromosome_to_expression(chromosome):
     return ''.join([ language[gene] for gene in chromosome ])
 
 def fitness(chromosome):
-   try:
-       return eval(chromosome_to_expression(chromosome))
-   except:
-       return 0
+    #print 'e:', chromosome_to_expression(chromosome)
+    score = 0
+    for c in combinations(chromosome):
+        try:
+            temp = eval(chromosome_to_expression(c))
+            length = len(c)#len(chromosome_to_expression(c))
+            #print 'length', length, chromosome_to_expression(c)
+            score += length
+        except:
+            pass 
+    return score
 
-genome = G1DList.G1DList(6)
+genome = G1DList.G1DList(4)
 genome.evaluator.set(fitness)
 genome.mutator.set(Mutators.G1DListMutatorIntegerRange)
-genome.setParams(rangemin=0, rangemax=len(language))
+genome.setParams(rangemin=0, rangemax=len(language)-1)
 ga = GSimpleGA.GSimpleGA(genome)
-ga.setMutationRate(.5)
-ga.setPopulationSize(100)
-ga.setGenerations(100)
-ga.evolve(freq_stats=10)
-print 'Best individual was the expression', chromosome_to_expression(ga.bestIndividual().genomeList), 'which equals', eval(chromosome_to_expression(ga.bestIndividual().genomeList))
-
-
+ga.setMutationRate(0.5)
+populationSize = 10
+ga.setPopulationSize(populationSize)
+ga.setGenerations(10)
+ga.evolve(freq_stats=1)
+pop = ga.getPopulation()
+pop.sort()
+index = 0
+good = 0
+while index < populationSize:
+    try:
+        print 'genome:', pop[index].genomeList
+        print 'trying ...'
+        print chromosome_to_expression(pop[index].genomeList)
+        print 'Evaluates to', eval(chromosome_to_expression(pop[index].genomeList))
+        index += 1
+        good = index
+    except:
+        print '\n Sorry, no more valid expressions'
+        index = populationSize+1
+print 'done, producing %i good statements' %good
