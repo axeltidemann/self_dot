d84d1de17ba75704353848159bdf5af0431028ea
Axel Tidemann <axel.tidemann@gmail.com>
2014-04-22 17:09:58 +0200

    Communication over sockets.

---
+++ communication.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+# -*- coding: latin-1 -*-
+
+'''Very simple communication module for [self.]
+
+@author: Axel Tidemann
+@contact: axel.tidemann@gmail.com
+@license: GPL
+
+Talk to [self.] over sockets.
+'''
+
+from __future__ import print_function
+
+import socket
+import sys
+
+def receive(callback, host='localhost', port=7777):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind((host, port))
+    sock.listen(1)
+    print('Communication channel listening on {}:{}'.format(host, port))
+    while True:
+        try:
+            connection, address = sock.accept()
+            data = connection.recv(1024)
+            callback(data)
+            connection.close()
+        except:
+            print('Communication channel going down.')
+            sock.close()
+            break
+    
+def send(message, host='localhost', port=7777):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((host, port))
+    sock.sendall(message)
+    sock.close()
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        for message in sys.argv[1:]:
+            send(message)
+    else:
+        receive(print)
+
