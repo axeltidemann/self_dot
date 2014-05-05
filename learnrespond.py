4cd7d064314baca72924efc36ffe8ae39ced46c9
Oeyvind_Brandtsegg <obrandts@gmail.com>
2014-05-05 09:57:04 +0200

    Small modification to add option of response without learning anything new.

--- learnrespond.py
+++ learnrespond.py
@@ -3,18 +3,22 @@ import sys
 
 from communication import send
 
-secs = int(sys.argv[1]) if len(sys.argv) == 2 else 5
+secs = int(sys.argv[1]) if len(sys.argv) >= 2 else 5
+mode = (sys.argv[2]) if len(sys.argv) >= 3 else 'both'
 
-send('startrec')
-time.sleep(secs)
-send('stoprec')
-send('learn')
+if mode == 'both':
+    print 'learning'
+    send('startrec')
+    time.sleep(secs)
+    send('stoprec')
+    send('learn')
+    print 'done learning'
+    time.sleep(5)
 
-time.sleep(5)
-
+print 'responding'
 send('startrec')
 time.sleep(secs)
 send('stoprec')
 send('respond')
 
-time.sleep(secs)
+#time.sleep(secs)
