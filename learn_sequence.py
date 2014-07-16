df2fdfc34196f9eb4e26ba52892646a3c4fc6e06
Axel Tidemann <axel.tidemann@gmail.com>
2014-07-16 23:18:03 +0200

    Learning of sequences.

--- learn_sequence.py
+++ learn_sequence.py
@@ -12,7 +12,7 @@ import Oger
 import mdp
 import numpy as np
 
-def _index_to_text(idxs, table):
+def index_to_text(idxs, table):
     return [ table[np.argmax(row)] for row in idxs ]
 
 def learn(sequence, freerun_steps=None, repeat=10):
@@ -39,13 +39,6 @@ def learn(sequence, freerun_steps=None, repeat=10):
 
     return flow, train_signals, unique
 
-    # Trying with random end word
-    test = train_signals[np.random.choice(len(sequence))]
-    print 'Start word: {}'.format(_index_to_text(np.atleast_2d(test), unique))
-    test_signals = np.tile(test, [freerun_steps+1,1])
-    return _index_to_text(flow(test_signals)[-freerun_steps:], unique)
-    
-    return _index_to_text(flow(train_signals)[-freerun_steps:], unique)
     
 if __name__ == '__main__':
     txt = open('sequence.txt')
@@ -54,25 +47,47 @@ if __name__ == '__main__':
         symbols.extend(line.split())
     net, train_signals, dictionary = learn(symbols)
 
-
     dummy = np.empty( (len(symbols), train_signals.shape[1]) )
-    dummy[:] = np.nan
+    dummy[:] = np.nan # NaNs spread like the plauge, we therefore use them to check for programming errors.
+
     print 'Regenerating entire learned sequence, feeding the entire sequence as input. This should yield stable behaviour.'
-    print _index_to_text(net(
+    dummy[0] = train_signals[0] # Due to a bug (?) in Oger, the first non-teacher forced signal must be manually set.
+    print index_to_text(net(
         np.vstack((train_signals, dummy)))[-len(symbols):],
         dictionary)
 
-    print 'Predicting rest of learned sequence. The network is fed up until this word, i.e. this is the input:'
+    print '\nPredicting rest of learned sequence. The network is fed up until this word, i.e. this is the input:'
     marker = np.random.choice(len(symbols))
     up_until = train_signals[:marker]
-    print _index_to_text(up_until, dictionary)
-    print _index_to_text(net(
+    print index_to_text(up_until, dictionary)
+    print 'This is the output:'
+    dummy[0] = train_signals[marker]
+    print index_to_text(net(
         np.vstack(( up_until, dummy )))[marker:len(symbols)],
         dictionary)
     
-    random_word = np.atleast_2d(train_signals[np.random.choice(len(symbols))])
-    print 'Starting with a randomly selected word, feeding just this one word to the network: {}\
-    This should yield unstable sequences.'.format(_index_to_text(random_word, dictionary))
-    print _index_to_text(net(
-        np.vstack((random_word, dummy)))[1:],
+    random_word = np.random.choice(len(symbols))
+    dummy[0] = train_signals[random_word]
+    print '\nStarting with a randomly selected word, feeding just this one word to the network: {}'.format(index_to_text(np.atleast_2d(train_signals[random_word]), dictionary))
+    print 'This should yield unstable sequences:'
+    print index_to_text(net(
+        np.vstack((np.atleast_2d(train_signals[random_word]), dummy)))[1:],
         dictionary)
+    
+    print '\nExample of secondary guesses of symbols. Tested to output entire sequence, but instead secondary guesses are displayed.'
+    dummy[0] = train_signals[0] 
+    output = net(np.vstack(( train_signals, dummy )))[-len(symbols):]
+    
+    for row in output:
+        row[np.argmax(row)] = np.min(row)
+
+    print index_to_text(output, dictionary)
+
+    print '\nExample of negated guesses of symbols. Tested to output entire sequence, but instead the least likely symbol is displayed.'
+    dummy[0] = train_signals[0] 
+    output = net(np.vstack(( train_signals, dummy )))[-len(symbols):]
+
+    for row in output:
+        row[np.argmin(row)] = np.max(row) + 1
+
+    print index_to_text(output, dictionary)
