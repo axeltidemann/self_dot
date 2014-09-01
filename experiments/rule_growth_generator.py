ef51bd083429b71bb5cc3e9edef75aafdaeb7553
Oeyvind <obrandts@gmail.com>
2014-09-01 22:55:44 +0200

    RuleGrowth generator

--- experiments/rule_growth_generator.py
+++ experiments/rule_growth_generator.py
@@ -11,7 +11,9 @@
 
 from __future__ import print_function
 import re
-from subprocess import call
+import random
+#from subprocess import call
+name_re = re.compile(r'\**\*:') 
 
 def makeDict(rulefile):
     '''
@@ -22,7 +24,6 @@ def makeDict(rulefile):
     '''
     d = {}
     txt = open(rulefile)
-    name_re = re.compile(r'\**\*:') 
     for line in txt.readlines():
         arrow = line.find('==>')
         support = line.find('#SUP')
@@ -48,19 +49,38 @@ def makeDict(rulefile):
 
 
 
-#def generateNext(word):
+def generateNext(word, ruleDict):
     '''
     the generator function should take a word, use as a key to the ruleDict,
     then select an item from the dict[key] value
     if the item is a word or word sequence, use it
     if the item is a name, use that name as key to the dict, and use the resulting value (words)
     '''
-    ##
-
+    if word not in ruleDict.keys():
+        print('no key', word, 'starting over')
+        word = random.choice(ruleDict.keys())
+        print('... with', word)
+        return word
+    candidates = ruleDict[word]
+    new = random.choice(candidates)
+    print('new', new)
+    if name_re.findall(new[0]):
+        print('found name', new, 'so trying again')
+        new = generateNext(new, ruleDict)
+    else:
+        return new
+        
 if __name__ == '__main__':
     db = 'association_test_db_short.txt'
     ruleDict = makeDict(db+'-output-rules-inverted')
     dictfile= open(db+'-dict', 'w+')
     for k,v in ruleDict.iteritems():
         dictfile.write("{}: {}\n".format(k,v))
-    
+    sentence = []
+    new = random.choice(ruleDict.keys())
+    sentence.append(new)
+    print('sentence is', sentence)
+    for i in range(10):
+        new = generateNext(new, ruleDict)
+        sentence.append(new)
+        print('sentence is', sentence)
