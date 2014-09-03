1bf43098009109bb12d731dc0ffea9451d7f67a7
Oeyvind <obrandts@gmail.com>
2014-09-03 23:55:53 +0200

    More work on weighted intersection. It is working technically, but still needs some adjustements to become interesting.

--- experiments/weightedSet_experiment1.py
+++ experiments/weightedSet_experiment1.py
@@ -32,29 +32,42 @@ def weightedIntersection(a_, b_, replacementValue):
         b = a_
     itemsA = [a[i][0] for i in range(len(a))]
     itemsB = [b[i][0] for i in range(len(b))]
-    itemsBcopy = copy.copy(itemsB)
     scoreA = normalize([a[i][1] for i in range(len(a))])
     scoreB = normalize([b[i][1] for i in range(len(b))])
+    removeFromB = []
     for i in range(len(itemsA)):
         itemA = itemsA[i]
         if itemA in itemsB:
             j = itemsB.index(itemA)
-            print itemsA[i], b[j], a[i] ### something can appparently go wrong here ###
             c.append([itemsA[i], b[j][1] * a[i][1]])    # multiply scores and put item in c
-            remIndex = itemsBcopy.index(itemA)          # update B, by...
-            b.remove(b[remIndex])                       # removing used elements...
-            itemsBcopy.remove(itemsBcopy[remIndex])     # and update the copy to keep indices consistent
+            removeFromB.append(j)                       # removing used elements...
         else:  
             c.append([itemsA[i], replacementValue])     # replacementValue may get a smarter implementation
-        for i in range(len(b)):
-            c.append([b[i][0], replacementValue])
+    removeFromB.sort()
+    removeFromB.reverse()
+    for i in removeFromB:
+        b.remove(b[i])
+    for i in range(len(b)):
+        c.append([b[i][0], replacementValue])
     return c
             
 def normalize(a):
-    highest = max(a)
-    for i in range(len(a)):
-        a[i] /= highest
+    if a != []:
+        highest = max(a)
+        if highest != 0:
+            for i in range(len(a)):
+                a[i] /= highest
     return a
+        
+def select(items, method):
+    words = [items[i][0] for i in range(len(items))]
+    scores = [items[i][1] for i in range(len(items))]
+    if method == 'highest':
+        return words[scores.index(max(scores))]
+    elif method == 'lowest':
+        return words[scores.index(min(scores))]
+    else:
+        random.choice(words)
         
 
 def generate(predicate):
@@ -64,18 +77,22 @@ def generate(predicate):
     similarWords = l.similarWords[predicate]
     # intersection
     temp = weightedIntersection(neighbors, wordsInSentence, 0)
+    temp = weightedIntersection(temp, similarWords, 0)
     # select the one with the highest score
-
+    nextWord = select(temp, 'highest')
+    return nextWord
+    
 ## testing
 if __name__ == '__main__':
-    l.importFromFile('minimal_db.txt')
+    l.importFromFile('association_test_db_full.txt')#minimal_db.txt')#roads_articulation_db.txt')#
     predicate = random.choice(list(l.words))
+    print 'predicate', predicate
     sentence = [predicate]
-    '''
-    for i in range(8):
+    for i in range(12):
         predicate = generate(predicate)
         sentence.append(predicate)
-        print sentence
+        print 'sentence', sentence
     '''
-    print generate(predicate)
-        
+    predicate = generate(predicate)
+    print 'generator', predicate
+    '''        
