93bfe9797bb00c6818681338216b8e3c51589c32
Oeyvind <obrandts@gmail.com>
2014-09-04 16:14:05 +0200

    More work on weighted sets. Something makes first set in a merge take over, even if it is set to have lower weight.

--- experiments/weightedSet_experiment1.py
+++ experiments/weightedSet_experiment1.py
@@ -19,31 +19,37 @@ allowing "query the dict for a weighted set of alternatives for items to associa
 
 import random
 import copy
+import numpy
+import math
 import loadDbWeightedSet as l
 
 
-def weightedSum(a_, b_,):
+def weightedSum(a_, weightA_, b_, weightB_):
     '''
     Add the score of items found in both sets,
     use score as is for items found in only one of the sets.
     '''
     c = []
     if len(a_) > len(b_):
-        a = a_
-        b = b_
+        a = copy.copy(a_)
+        weightA = weightA_
+        b = copy.copy(b_)
+        weightB = weightB_
     else:
-        a = b_
-        b = a_
+        a = copy.copy(b_)
+        weightA = weightB_
+        b = copy.copy(a_)
+        weightB = weightA_        
     itemsA = [a[i][0] for i in range(len(a))]
     itemsB = [b[i][0] for i in range(len(b))]
-    scoreA = normalize([a[i][1] for i in range(len(a))])
-    scoreB = normalize([b[i][1] for i in range(len(b))])
+    scoreA = normScale([a[i][1] for i in range(len(a))],weightA)
+    scoreB = normScale([b[i][1] for i in range(len(b))],weightB)
     removeFromB = []
     for i in range(len(itemsA)):
         itemA = itemsA[i]
         if itemA in itemsB:
             j = itemsB.index(itemA)
-            c.append([itemsA[i], b[j][1] + a[i][1]])    # add scores and put item in c
+            c.append([itemsA[i], scoreB[j] + scoreA[i]])    # add scores and put item in c
             removeFromB.append(j)                       # removing used elements...
         else:
             c.append([itemsA[i], scoreA[i]])         
@@ -52,50 +58,69 @@ def weightedSum(a_, b_,):
     for i in removeFromB:
         b.remove(b[i])
     if len(b) > 0:
-            c.append(b[i])
+            c.append([b[i][0], b[i][1]*weightB])
     return c
-            
-def weightedMultiply(a_, b_):
+          
+def defaultScale(x):
     '''
-    Multiply the score of items found in both sets,
-    multiply score with itself for items found in only one of the sets.
+    For use in weightedMultiply.
+    For now, we use an empirical adjustment trying to achive values in a range that could be produced by a*b
+    and also stay within approximately the same average range as random*random*0.5
+    '''
+    return (math.sqrt(x*0.1)+math.pow(x*0.5, 2))*0.5
+    
+def weightedMultiply(a_, weightA_, b_, weightB_):
+    '''
+    Multiply the score of items found in both sets.
+    If an item is found only in one of the sets, we need to find a way to scale the value appropriately,
+    see defaultScale for more info
     '''
     c = []
     if len(a_) > len(b_):
-        a = a_
-        b = b_
+        a = copy.copy(a_)
+        weightA = weightA_
+        b = copy.copy(b_)
+        weightB = weightB_
     else:
-        a = b_
-        b = a_
+        a = copy.copy(b_)
+        weightA = weightB_
+        b = copy.copy(a_)
+        weightB = weightA_        
     itemsA = [a[i][0] for i in range(len(a))]
     itemsB = [b[i][0] for i in range(len(b))]
-    scoreA = normalize([a[i][1] for i in range(len(a))])
-    scoreB = normalize([b[i][1] for i in range(len(b))])
+    scoreA = normScale([a[i][1] for i in range(len(a))],weightA)
+    scoreB = normScale([b[i][1] for i in range(len(b))],weightB)
     removeFromB = []
     for i in range(len(itemsA)):
         itemA = itemsA[i]
         if itemA in itemsB:
             j = itemsB.index(itemA)
-            c.append([itemsA[i], b[j][1] * a[i][1]])    # multiply scores and put item in c
+            c.append([itemsA[i], scoreB[j] * scoreA[i]])    # multiply scores and put item in c
             removeFromB.append(j)                       # removing used elements...
         else:
-            c.append([itemsA[i], scoreA[i]*scoreA[i]])   
+            c.append([itemsA[i], defaultScale(scoreA[i])])   
     removeFromB.sort()
     removeFromB.reverse()
     for i in removeFromB:
         b.remove(b[i])
     if len(b) > 0:
-        c.append([b[i][0], b[i][1]*b[i][1]])
+        c.append([b[i][0], defaultScale(b[i][1]*weightB)])
     return c
 
 def normalize(a):
     if a != []:
-        highest = max(a)
+        highest = float(max(a))
         if highest != 0:
             for i in range(len(a)):
                 a[i] /= highest
     return a
         
+def normScale(a, scale):
+    a = normalize(a)
+    a = list(numpy.array(a)*scale)
+    return a
+
+
 def select(items, method):
     words = [items[i][0] for i in range(len(items))]
     scores = [items[i][1] for i in range(len(items))]
@@ -107,52 +132,54 @@ def select(items, method):
         random.choice(words)
         
 
-def generate(predicate):
+def generate(predicate, method):
     # get the lists we need
     neighbors = l.neighbors[predicate]
     wordsInSentence = l.wordsInSentence[predicate]
     similarWords = l.similarWords[predicate]
-    print 'lengths', len(neighbors), len(wordsInSentence), len(similarWords)
-    
-    # multiply (soft intersection)
-    temp = weightedMultiply(neighbors, wordsInSentence)
-    print 'templength', len(temp)
-    temp = weightedMultiply(temp, similarWords)
-    print 'templength', len(temp)
-    '''
-    # add (union)
-    temp = weightedSum(neighbors, wordsInSentence)
-    print 'templength', len(temp)
-    temp = weightedSum(temp, similarWords)
-    print 'templength', len(temp)
-    '''
+    neighborsWeight = 0.001
+    wordsInSentenceWeight = 0.0000001
+    similarWordsWeight = 1.0
+    #print 'lengths', len(neighbors), len(wordsInSentence), len(similarWords)
+    if method == 'multiply':
+        # multiply (soft intersection)
+        temp = weightedMultiply(neighbors, neighborsWeight, wordsInSentence, wordsInSentenceWeight)
+        #print 'templength', len(temp)
+        temp = weightedMultiply(temp, 1.0, similarWords, similarWordsWeight)
+        #print 'templength', len(temp)
+    if method == 'add':
+        # add (union)
+        temp = weightedSum(neighbors, neighborsWeight, wordsInSentence, wordsInSentenceWeight)
+        #print 'templength', len(temp)
+        temp = weightedSum(temp, 1.0, similarWords, similarWordsWeight)
+        #print 'templength', len(temp)
     # select the one with the highest score
     nextWord = select(temp, 'highest')
     return nextWord
     
-def testSentence():
+def testSentence(method):
     l.importFromFile('association_test_db_full.txt')#minimal_db.txt')#roads_articulation_db.txt')#
     predicate = 'hurricane'#random.choice(list(l.words))
     print 'predicate', predicate
     sentence = [predicate]
     for i in range(12):
-        predicate = generate(predicate)
+        predicate = generate(predicate, method)
         sentence.append(predicate)
         print 'sentence', sentence
 
 def testMerge():
-    a = [['world', 0.6],['you', 0.5], ['there', 0.3]]
-    b = [['world', 0.3],['you', 0.5], ['there', 0.2],['here', 0.8]]
+    a = [['world', 0.7],['you', 0.5], ['there', 0.3]]
+    b = [['world', 0.3],['you', 0.5], ['there', 0.2],['here', 0.8],['nowhere',0.1]]
     print '** sum **'
-    c = weightedSum(a,b)
+    c = weightedSum(a,1,b,1)
     for item in c:
         print item
     print '** multiply **'
-    c = weightedMultiply(a,b)
+    c = weightedMultiply(a,1,b,1)
     for item in c:
         print item
 
 ## testing
 if __name__ == '__main__':
-    #testSentence()
-    testMerge()
+    testSentence('add')
+    #testMerge()
