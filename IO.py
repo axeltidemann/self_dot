bab21be85ae2bcd2b5193bdf65cadb8b86e6ecb8
Oeyvind <obrandts@gmail.com>
2014-11-14 15:24:43 +0100

    Better face response logic

--- IO.py
+++ IO.py
@@ -158,6 +158,8 @@ def audio():
         memRecTimeMarker = cGet("memRecTimeMarker")     # (in memRec) get the time since start of statement
         memRecActive = cGet("memRecActive")             # flag to check if memoryRecording is currently recording to file in Csound
         memRecMaxAmp = cGet("memRecMaxAmp")             # max amplitude for each recorded file
+        statusRelease = cGet("statusRel")               # audio status release time, hold status=1 for this long after signal has dropped below thresh, we subtract this from the total time recorded in each memoryRecording file
+        
         panposition = cs.GetChannel("panalyzer_pan")
         in_amp = cs.GetChannel("in_amp")
         in_pitch = cs.GetChannel("in_pitch")
@@ -234,7 +236,7 @@ def audio():
                 segmentstring += '%.3f %.3f %.3f %.3f\n'%(segStart,ampMean,pitchMean,centroidMean)
                 cs.InputMessage('i -34 0 1')
                 markerfile.write(segmentstring)
-                markerfile.write('Total duration: %f\n'%memRecTimeMarker)
+                markerfile.write('Total duration: %f\n'%memRecTimeMarker-statusRelease)
                 markerfile.write('\nMax amp for file: %f'%memRecMaxAmp)
                 markerfile.close()
                 print 'stopping memoryRec'
@@ -255,7 +257,7 @@ def audio():
             segmentstring += '%.3f %.3f %.3f %.3f\n'%(segStart,ampMean,pitchMean,centroidMean)
             cs.InputMessage('i -34 0 1')
             markerfile.write(segmentstring)
-            markerfile.write('Total duration: %f'%memRecTimeMarker)
+            markerfile.write('Total duration: %f'%memRecTimeMarker-statusRelease)
             markerfile.close()
             print 'stopping memoryRec'
             #assoc.send_json(markerfileName)
