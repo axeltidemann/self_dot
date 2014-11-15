6350cc70d7f0f129f2cd7a4b617bb48598e3b790
Oeyvind <obrandts@gmail.com>
2014-11-16 00:38:55 +0100

    new segmentation, skip all silence

--- IO.py
+++ IO.py
@@ -159,7 +159,7 @@ def audio():
         memRecTimeMarker = cGet("memRecTimeMarker")     # (in memRec) get the time since start of statement
         memRecActive = cGet("memRecActive")             # flag to check if memoryRecording is currently recording to file in Csound
         memRecMaxAmp = cGet("memRecMaxAmp")             # max amplitude for each recorded file
-        statusRelease = cGet("statusRel")               # audio status release time, hold status=1 for this long after signal has dropped below thresh, 
+        #statusRelease = cGet("statusRel")               # audio status release time, hold status=1 for this long after signal has dropped below thresh, 
                                                         # we subtract this from the total time recorded in each memoryRecording file,
                                                         # to compensate for the start of recording being delayed by the same amount.
                                                         # This let us skip the silent section that would otherwise be recorded at the end of each file.
@@ -244,7 +244,7 @@ def audio():
                 segmentstring += '%.3f %.3f %.3f %.3f\n'%(segStart,ampMean,pitchMean,centroidMean)
                 cs.InputMessage('i -34 0 1')
                 markerfile.write(segmentstring)
-                markerfile.write('Total duration: %f\n'%(memRecTimeMarker-statusRelease))
+                markerfile.write('Total duration: %f\n'%memRecTimeMarker)
                 markerfile.write('\nMax amp for file: %f'%memRecMaxAmp)
                 markerfile.close()
                 print 'stopping memoryRec'
@@ -264,7 +264,7 @@ def audio():
             segmentstring += '%.3f %.3f %.3f %.3f\n'%(segStart,ampMean,pitchMean,centroidMean)
             cs.InputMessage('i -34 0 1')
             markerfile.write(segmentstring)
-            markerfile.write('Total duration: %f'%(memRecTimeMarker-statusRelease))
+            markerfile.write('Total duration: %f'%memRecTimeMarker)
             markerfile.close()
             print 'stopping memoryRec'
 
